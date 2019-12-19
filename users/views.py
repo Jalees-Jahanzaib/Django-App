afd297549a0da82af6bd5f6ad50bc02c904018e0
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-19 17:03:39 +0530

    7th Completed

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
     if request.method=='POST':
@@ -17,5 +18,6 @@ def register(request):
 
         
     return render(request,'users/register.html',{'form':form})
+@login_required   
 def profile(request):
-    return render(request,'user/profile.html')
+    return render(request,'users/profile.html')
