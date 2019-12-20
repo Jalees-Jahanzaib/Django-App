447abe4f629a5b3068e1666d878ed5f4257ec483
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-20 15:53:42 +0530

    added files

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
 
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+
